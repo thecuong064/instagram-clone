747afa6aec938faf64e5de61c3d5ed7c964fee4a
thecuong064 <laithecuong8@gmail.com>
2022-02-20 13:44:47 +0700

    setup Axios and add samples for Redux and Redux sagas implementation

--- src/redux/sagas/rootSaga.js
+++ src/redux/sagas/rootSaga.js
@@ -1,5 +1,6 @@
 import {all, fork} from 'redux-saga/effects';
+import ApiPractiesSagas from '../ApiPracties/sagas';
 
 export default function* rootSaga() {
-  yield all([fork()]);
+  yield all([fork(ApiPractiesSagas)]);
 }
